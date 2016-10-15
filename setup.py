17229183833c421f1dce943aba65944b44dd3c45
Andrey Bukati <andrey@bukati.me>
2016-10-15 14:35:18 +0300

    gitignore updated

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+        name='RapidConnect',
+        version='0.0.1',
+        author='Andrey Bukati',
+        author_email='andrey@rapidapi.com',
+        description='Official Python client for RapidAPI Connect',
+        url='https://github.com/RapidSoftwareSolutions/RapidAPIConnect_Python',
+        keywords=['rapidapi', 'api', 'python'],
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 3',
+        ],
+        package_dir={'': '.'},
+        packages=find_packages('.'),
+        install_requires=[
+            'requests>=2.8.1',
+            'six>=1.10.0',
+        ],
+)
