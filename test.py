8947677049c7e7120c66557519e6301cf4ecca24
Andrey Bukati <andrey@bukati.me>
2016-11-13 10:59:17 +0200

    files changed

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from rapidconnect import RapidConnect
+import logging
 
-rapid = RapidConnect("giladProject", "72466c25-98a1-46a5-bf88-e711deda6b5b")
+logging.info("TEST")
 
-result = rapid.call("GoogleTranslate", "translate", {
-    'string': 'שלום',
-    'sourceLanguage': 'he',
-    'targetLanguage': 'en',
-    'apiKey': 'XXXX'
+rapid = RapidConnect('lightbringer', '3dab63eb-4166-4315-81f5-f7c79164d31f');
+result = rapid.call('MicrosoftComputerVision', 'describeImage', {
+    'image': 'https://s15.postimg.org/id184l9t7/image.jpg',
+    'subscriptionKey': 'a04b26d206e2482083db92c60b3b818a',
+    'maxCandidates': ''
 
-})
-
+});
 print result
