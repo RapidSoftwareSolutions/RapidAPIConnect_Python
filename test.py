ae482e142e53ef5a27b3e94fe1d54e065f10dce6
Spyes <lewis@rapidapi.com>
2017-02-23 17:52:11 +0200

    webhook events

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 
 from rapidconnect import RapidConnect
 
-rapid = RapidConnect("giladProject", "72466c25-98a1-46a5-bf88-e711deda6b5b")
+rapid = RapidConnect("testa", "a3787239-bb1e-4fa4-85c3-a423fa6af51f")
 
 result = rapid.call("GoogleTranslate", "translate", {
     'string': 'שלום',
@@ -13,3 +13,20 @@ result = rapid.call("GoogleTranslate", "translate", {
 })
 
 print result
+
+def on_join():
+    print("joined!")
+
+def on_message(message):
+    print(message)
+
+def on_close():
+    print("Closed!")
+
+def on_error(message):
+    print("error:")
+    print(message)
+    
+res = rapid.listen("Slack", "slashCommand", {"token": "ydt3vFyVEoW51ZFCC2i5QKab", "command": "/send_test"}, on_join=on_join, on_message=on_message, on_close=on_close)
+j = res.json()
+print j["token"]
