f128491df0d353feeed91d18b6da517bdd4698fb
Lewis Nitzberg <lnitzberg@gmail.com>
2017-03-13 15:43:23 +0200

    Update test.py

--- test.py
+++ test.py
@@ -13,20 +13,3 @@ result = rapid.call("GoogleTranslate", "translate", {
 })
 
 print result
-
-def on_join():
-    print("joined!")
-
-def on_message(message):
-    print(message)
-
-def on_close():
-    print("Closed!")
-
-def on_error(message):
-    print("error:")
-    print(message)
-    
-res = rapid.listen("Slack", "slashCommand", {"token": "ydt3vFyVEoW51ZFCC2i5QKab", "command": "/send_test"}, on_join=on_join, on_message=on_message, on_close=on_close)
-j = res.json()
-print j["token"]
